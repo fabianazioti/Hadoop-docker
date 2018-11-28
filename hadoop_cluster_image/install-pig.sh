bf33add32e1f7cdfbe8451c769aae474856f31c8
Fabiana Zioti <fabiana_zioti@hotmail.com>
2018-11-28 14:06:32 -0200

    updating files and add pig install

---
+++ hadoop_cluster_image/install-pig.sh
@@ -0,0 +1,32 @@
+# Install PIG
+
+#!/bin/bash
+
+mkdir -p ~/pig/
+
+cd pig
+
+wget http://www-us.apache.org/dist/pig/pig-0.16.0/pig-0.16.0.tar.gz
+
+tar -xzf pig-0.16.0.tar.gz 
+
+rm pig-0.16.0.tar.gz
+
+# arquivo .bashrc add variaveis
+
+nano ~/.bashrc
+
+export PIG_HOME="/root/pig/pig-0.16.0"
+export PIG_CONF_DIR="$PIG_HOME/conf"
+export PIG_CLASSPATH="$PIG_CONF_DIR"
+export PATH="$PIG_HOME/bin:$PATH"
+
+source .bashrc
+
+RUN     echo "PATH=$PATH:$HADOOP_HOME/bin" >> ~/.bashrc
+
+
+echo "PIG_HOME=$PIG_HOME:/root/pig/pig-0.16.0" >> ~/.bashrc
+echo "PIG_CONF_DIR=$PIG_CONF_DIR:$PIG_HOME/conf" >> ~/.bashrc
+echo "PIG_CLASSPATH=$PIG_CLASSPATH:$PIG_CONF_DIR" >> ~/.bashrc
+echo "PATH=$PATH:$PIG_HOME/bin:$PATH" >> ~/.bashrc
