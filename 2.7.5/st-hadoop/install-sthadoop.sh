535903c74d904f0fdd3893f461c54e38fe960511
Fabiana Zioti <fabiana_zioti@hotmail.com>
2019-03-13 10:47:14 -0300

    updating spatial-hadoop install file

--- 2.7.5/st-hadoop/install-sthadoop.sh
+++ 2.7.5/st-hadoop/install-sthadoop.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 
+wget http://ftp.unicamp.br/pub/apache/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
+tar xzvf apache-maven-3.6.0-bin.tar.gz 
+mv apache-maven-3.6.0 /opt/ 
+rm apache-maven-3.6.0-bin.tar.gz
+
+export PATH=/opt/apache-maven-3.6.0/bin:$PATH
+
 mkdir -p  ~/sthadoop/
-
 cd ~/sthadoop/
+git clone https://github.com/fabianazioti/st-hadoop.git 
+cd st-hadoop 
 
-git clone https://github.com/fabianazioti/st-hadoop.git
+mvn compile 
+mvn assembly:assembly 
+mv ./target/sthadoop-2.4.1-SNAPSHOT-uber.jar ./target/st-hadoop-uber.jar
 
-cd st-hadoop ./compile_local.sh
+scp ./target/st-hadoop-uber.jar hadoop-slave1:/root
+scp ./target/sthadoop-2.4.1-SNAPSHOT-sources.jar hadoop-slave1:/root
+
+scp ./target/st-hadoop-uber.jar hadoop-slave2:/root
+scp ./target/sthadoop-2.4.1-SNAPSHOT-sources.jar hadoop-slave2:/root
+
+
